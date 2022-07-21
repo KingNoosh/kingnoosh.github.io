48a2d41d51829c5a61613fb6fb69ca54f85751fb
Anosh Malik <9887966+KingNoosh@users.noreply.github.com>
2022-07-21 03:09:45 +0000

    Update padding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,15 @@ import { SocialIcon } from "react-social-icons"
 export default function IndexPage() {
   return (
     <main>
-      <div className="grid h-screen place-items-center">
+      <div className="grid h-screen place-items-center p-5">
         <div>
           <div className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-500 to-rose-500">
             Anosh Malik
           </div>
-          <div className="grid gap-1 grid-cols-12">
-            <SocialIcon url="https://www.linkedin.com/in/anoshmalik" target={"_blank"} />
-            <SocialIcon url="https://github.com/KingNoosh" target={"_blank"} />
-            <SocialIcon url="https://twitter.com/KingNoosh" target={"_blank"} />
+          <div className="flex flex-1">
+            <SocialIcon className="mr-1" url="https://www.linkedin.com/in/anoshmalik" target={"_blank"} />
+            <SocialIcon className="mr-1" url="https://github.com/KingNoosh" target={"_blank"} />
+            <SocialIcon className="mr-1" url="https://twitter.com/KingNoosh" target={"_blank"} />
           </div>
         </div>
       </div>
